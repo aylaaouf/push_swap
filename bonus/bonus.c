a142213764809e76dc76e196cf3eb2d47cb9e9b8
Ayoub Laaoufi <ayoub.laaoufi@hotmail.com>
2025-01-27 20:21:54 +0100

    last update

--- bonus/bonus.c
+++ bonus/bonus.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 05:01:53 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/01/26 07:05:49 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/01/27 20:20:26 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,32 +34,32 @@ void	error(char *line, t_stack_node *stack_a, t_stack_node *stack_b)
 	exit(1);
 }
 
-void	operations(char *line, t_stack_node *stack_a, t_stack_node *stack_b)
+void	operations(char *line, t_stack_node **stack_a, t_stack_node **stack_b)
 {
 	if (ft_strncmp(line, "sa\n", 3) == 0)
-		sa(&stack_a, 0);
+		sa(stack_a, 0);
 	else if (ft_strncmp(line, "sb\n", 3) == 0)
-		sb(&stack_b, 0);
+		sb(stack_b, 0);
 	else if (ft_strncmp(line, "ss\n", 3) == 0)
-		ss(&stack_a, &stack_b, 0);
+		ss(stack_a, stack_b, 0);
 	else if (ft_strncmp(line, "pa\n", 3) == 0)
-		pa(&stack_a, &stack_b, 0);
+		pa(stack_a, stack_b, 0);
 	else if (ft_strncmp(line, "pb\n", 3) == 0)
-		pb(&stack_a, &stack_b, 0);
+		pb(stack_a, stack_b, 0);
 	else if (ft_strncmp(line, "ra\n", 3) == 0)
-		ra(&stack_a, 0);
+		ra(stack_a, 0);
 	else if (ft_strncmp(line, "rb\n", 3) == 0)
-		rb(&stack_b, 0);
+		rb(stack_b, 0);
 	else if (ft_strncmp(line, "rr\n", 3) == 0)
-		rr(&stack_a, &stack_b, 0);
+		rr(stack_a, stack_b, 0);
 	else if (ft_strncmp(line, "rra\n", 4) == 0)
-		rra(&stack_a, 0);
+		rra(stack_a, 0);
 	else if (ft_strncmp(line, "rrb\n", 4) == 0)
-		rrb(&stack_b, 0);
+		rrb(stack_b, 0);
 	else if (ft_strncmp(line, "rrr\n", 4) == 0)
-		rrr(&stack_a, &stack_b, 0);
+		rrr(stack_a, stack_b, 0);
 	else
-		error(line, stack_a, stack_b);
+		error(line, *stack_a, *stack_b);
 }
 
 int	main(int ac, char **av)
@@ -78,7 +78,7 @@ int	main(int ac, char **av)
 	line = get_next_line(0);
 	while (line)
 	{
-		operations(line, stack_a, stack_b);
+		operations(line, &stack_a, &stack_b);
 		free(line);
 		line = get_next_line(0);
 	}
